ed48afe1eaf967e8f0c81d66c8e5b5f35cd52465
vszealot <vszealot@gmail.com>
2018-11-27 17:57:27 +0900

    add Hunting_spot.java, hunting_spot.xml mod Area.java, AndroidManifest.xml, build.gradle

--- app/src/main/java/com/example/jypark/gazua/Area.java
+++ app/src/main/java/com/example/jypark/gazua/Area.java
@@ -1,5 +1,6 @@
 package com.example.jypark.gazua;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,7 +15,23 @@ View toastView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area);
 
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        button4 = (Button)findViewById(R.id.button4);
+        button5 = (Button)findViewById(R.id.button5);
+        button6 = (Button)findViewById(R.id.button6);
+        button7 = (Button)findViewById(R.id.button7);
 
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Hunting_spot.class);
+                intent.putExtra("x",37.566481);
+                intent.putExtra("y",126.977914);
+                startActivityForResult(intent, 0);
+            }
+        });
 
        /* button1.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/jypark/gazua/Hunting_spot.java
@@ -0,0 +1,59 @@
+package com.example.jypark.gazua;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Hunting_spot extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hunting_spot);
+
+        // 위성, 지형, 일반 지도
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.setMyLocationEnabled(true);
+        map.setTrafficEnabled(true);
+        map.setIndoorEnabled(true);
+        map.setBuildingsEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true);
+
+        final Intent intent = getIntent();
+        double a = intent.getDoubleExtra("x",0);
+        double b = intent.getDoubleExtra("y",0);
+
+        //map.google.com 마커 찍기
+        final LatLng LOC = new LatLng(a, b);
+/*
+        map.addMarker(new MarkerOptions().position(LOC));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(LOC, 16));
+
+        map.addMarker(
+                new MarkerOptions()
+                        .position(LOC)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                        //.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker))
+                        .title("커피향기")
+                        .snippet("아메리카노+프레즐"));
+        */
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(LOC, 12));
+    }
+
+}
