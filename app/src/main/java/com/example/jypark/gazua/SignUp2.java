07f7990c944871378302bafaeff4ad00dd8951af
soulsick <chokobo86@me.com>
2018-12-14 18:10:59 +0900

    기본 login 빼고 삭제

--- app/src/main/java/com/example/jypark/gazua/SignUp2.java
+++ app/src/main/java/com/example/jypark/gazua/SignUp2.java
@@ -11,13 +11,24 @@ import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class SignUp2 extends DialogFragment {
     String sNickname, sNationality, sDetail, sPhoto="basic";
-    EditText NICKNAME, NATIONALITY, DETAIL, PHOTO;
+    String[] item;
+    Spinner spinner;
+    EditText NICKNAME, DETAIL, PHOTO;
+    TextView NATIONALITY;
     HashMap<String,String> map = null;
 
     @NonNull
@@ -28,7 +39,30 @@ public class SignUp2 extends DialogFragment {
         View view = inflater.inflate(R.layout.activity_signup2, null);
 
         NICKNAME = view.findViewById(R.id.NICKNAME);
-        NATIONALITY = view.findViewById(R.id.NATIONALITY);
+        spinner = view.findViewById(R.id.NATIONALITY);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        item = new String[Locale.getISOCountries().length];
+        for( int i=0; i < item.length; i++ ){
+            item[i] = new Locale("en", Locale.getISOCountries()[i]).getDisplayCountry();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
         DETAIL = view.findViewById(R.id.DETAIL);
         PHOTO = view.findViewById(R.id.PHOTO);
 
@@ -40,7 +74,7 @@ public class SignUp2 extends DialogFragment {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
                         sNickname = NICKNAME.getText().toString();
-                        sNationality = NATIONALITY.getText().toString();
+                        sNationality = spinner.toString();
                         sDetail = DETAIL.getText().toString();
                         sPhoto = PHOTO.getText().toString();
 
